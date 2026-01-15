c1c67d9edd2699d4537bf182a9c1c1602c418f42
Stimul88 <ivanov2288@gmail.com>
2026-01-15 20:24:42 +0300

    events fix

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
+import kotlin.math.floor
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +26,13 @@ class MainActivity : AppCompatActivity() {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
             published = "21 мая в 18:36",
-            likes = 999,
+            likes = 1099,
             likedByMe = false,
-            countShare = 1099,
+            countShare = 10395,
             countEye = 1100
         )
+
+
 
 
         with(binding) {
--- app/src/main/java/ru/netology/nmedia/countFunction.kt
+++ app/src/main/java/ru/netology/nmedia/countFunction.kt
@@ -1,9 +1,11 @@
 package ru.netology.nmedia
 
+import android.annotation.SuppressLint
+
+@SuppressLint("DefaultLocale")
 fun countFunction (count: Int) = when(count) {
     in 0..999 -> count.toInt().toString()
-    in 1000 .. 9999 -> String.format("%.2f", (count/1000.0)).dropLast(1).trimEnd('0','.').plus('K')
-    in 10000 .. 999999 -> String.format("%d", (count/1000.0).toInt()).plus('K')
+    in 1000 .. 9999 -> String.format("%.3f", (count/1000.0)).dropLast(2).trimEnd('0','.').plus('K')
+    in 10000 .. 999999 ->String.format("%d", (count/1000.0).toInt()).plus('K')
     else -> String.format("%.2f", (count/1000000.0)).dropLast(1).trimEnd('0','.').plus('M')
 }
-
